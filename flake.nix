025008fccf46fd1b2ddc447f66c73f923b3191f1
matus.benko <matus.benko@gmail.com>
2023-10-16 10:26:35 +0200

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Shell for compiling qmk";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = inputs@{ self, nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell rec {
+          name = "nix.shell.qmk.primamateria";
+          packages = with pkgs; [
+            gnumake
+            gcc
+            gcc-arm-embedded
+          ];
+
+          shellHook = ''
+            alias left="make CONVERT_TO=promicro_rp2040 ferris/sweep:sweep:primamateria:uf2-split-left"
+            alias right="make CONVERT_TO=promicro_rp2040 ferris/sweep:sweep:primamateria:uf2-split-right"
+            alias edit="nvim ./keyboards/ferris"
+
+            echo "${name} started"
+          '';
+        };
+      });
+}
--- keyboards/ferris/keymaps/primamateria/readme.md
+++ keyboards/ferris/keymaps/primamateria/readme.md
@@ -1,4 +1,9 @@
 ```
-make CONVERT_TO=promicro_rp2040 ferris/sweep:sweep:primamateria:utf2-split-left
-make CONVERT_TO=promicro_rp2040 ferris/sweep:sweep:primamateria:utf2-split-right
+make CONVERT_TO=promicro_rp2040 ferris/sweep:sweep:primamateria:uf2-split-left
+make CONVERT_TO=promicro_rp2040 ferris/sweep:sweep:primamateria:uf2-split-right
 ```
+
+It should be enough to copy the produced uf2 files to the usb drive that appears
+when the board the board is reset with the bootloader.
+
+The bitlocker will unfortunately allow to mount it read-only.
