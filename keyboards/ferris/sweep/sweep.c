61ca05219d0af853a0dca875d1c1ab09e9944c26
matus.benko <matus.benko@gmail.com>
2023-10-21 23:02:33 +0200

    caps word keep dash

--- keyboards/ferris/sweep/sweep.c
+++ keyboards/ferris/sweep/sweep.c
@@ -22,3 +22,21 @@ const keypos_t PROGMEM hand_swap_config[MATRIX_ROWS][MATRIX_COLS] = {
 };
 #endif
 
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
+            return true;
+
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_UNDS:
+            return true;
+
+        default:
+            return false;  // Deactivate Caps Word.
+    }
+}
